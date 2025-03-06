9f22f707078743b5d89ed9cd12c43e7d707e6d75
taylor <taylorking206@u.boisestate.edu>
2025-03-05 21:15:14 -0700

    add remove drag onto layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,25 @@
 <script>
 	import { Struct } from 'drizzle-struct/front-end';
 	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	setTimeout(() => {
 		if (browser) Struct.buildAll();
 	});
 	const { children } = $props();
+
+	onMount(() => {
+		document.addEventListener('dragstart', e => e.preventDefault());
+	});
 </script>
 
 <main>
 	{@render children()}
 </main>
+
+<style>
+	* {
+		user-drag: none;
+		-webkit-user-drag: none;
+	}
+</style>
