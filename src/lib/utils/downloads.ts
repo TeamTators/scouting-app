8de7095cc4f83b41d3dd430be8a1118d9608a5b4
Sylvie Rupe <raygenrrupe@gmail.com>
2025-03-14 15:25:21 -0600

    actually enable multiple file select

--- src/lib/utils/downloads.ts
+++ src/lib/utils/downloads.ts
@@ -50,7 +50,7 @@ export const loadFiles = (): Promise<Result<FileList>> => {
 		return new Promise<FileList>((res, rej) => {
 			const element = document.createElement('input');
 			element.setAttribute('type', 'file');
-			element.setAttribute('multiple', 'multiple');
+			element.setAttribute('multiple', '');
 			element.style.display = 'none';
 			document.body.appendChild(element);
 			element.click();
