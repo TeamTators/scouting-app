3b3de7fa7b6c11027e80ca0d555a715cf39a5eb2
tsaxking <taylorking206@u.boisestate.edu>
2026-04-08 22:43:16 +0000

    random ports

---
+++ scripts/supabase/sb-random-ports.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import fs from 'fs';
+import { str } from '../../src/lib/server/utils/env';
+
+export default () => {
+    const studio = 54323;
+    const mailpit = 54324;
+    const mcp = 54321;
+    const postgres = 54322;
+
+    const max = 65535 - Math.max(studio, mailpit, mcp, postgres);
+
+    const add = Math.floor(Math.random() * max);
+
+
+    const files = [
+        path.join(process.cwd(), str('CONFIG_PATH', true)),
+        path.join(process.cwd(), 'supabase', 'config.toml'),
+    ];
+
+    for (const file of files) {
+        let content = fs.readFileSync(file, 'utf-8');
+        for (const port of [studio, mailpit, mcp, postgres]) {
+            const newPort = port + add;
+            content = content.replaceAll(port.toString(), newPort.toString());
+        }
+        fs.writeFileSync(file, content);
+    }
+};
