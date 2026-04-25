6bb22ddfb0f052714b7f737fc9af606409b84fef
tsaxking <taylorking206@u.boisestate.edu>
2026-04-25 19:47:25 +0000

    allow localhost

--- src/lib/server/utils/config.ts
+++ src/lib/server/utils/config.ts
@@ -93,7 +93,7 @@ export default z.object({
 		public_key: z.string().min(1),
 		schema: z.string().min(1),
 		secret_key: z.string().min(1),
-		local_ip: z.string().ip(),
+		local_ip: z.union([z.string().ip(), z.literal('localhost')]),
 		db_port: z.number().min(1).max(65535),
 		sb_pass: z.string()
 	}),
