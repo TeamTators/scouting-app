a88ae92b192a53dafc1c2e083d25781a9365ae62
tsaxking <taylorking206@u.boisestate.edu>
2026-04-25 22:51:47 +0000

    realtime -> crud

--- src/tests/crud.test.ts
+++ src/tests/crud.test.ts
@@ -5,7 +5,6 @@
 import supabase from '$lib/server/services/supabase';
 import { SupaStruct } from '$lib/services/supabase/supastruct';
 import { describe, it, expect } from 'vitest';
-import { SupaStructData } from '$lib/services/supabase/supastruct-data';
 
 describe('Realtime Tests', () => {
 	const struct = SupaStruct.get({
@@ -17,36 +16,6 @@ describe('Realtime Tests', () => {
 	struct.setupListeners();
 
 	it('Should run the create-update-delete flow', async () => {
-		await new Promise<void>((res) =>
-			struct.on('realtime', (status) => {
-				if (status === 'SUBSCRIBED') setTimeout(res, 200);
-			})
-		);
-		const createPromise = new Promise<SupaStructData<'test'>>((res, rej) => {
-			struct.on('new', (data) => {
-				res(data);
-			});
-			setTimeout(() => {
-				rej(new Error('Did not receive create event within 10 seconds'));
-			}, 10000);
-		});
-		const updatePromise = new Promise<SupaStructData<'test'>>((res, rej) => {
-			struct.on('update', (data) => {
-				res(data);
-			});
-			setTimeout(() => {
-				rej(new Error('Did not receive update event within 10 seconds'));
-			}, 20000);
-		});
-		const deletePromise = new Promise<SupaStructData<'test'>>((res, rej) => {
-			struct.on('delete', (data) => {
-				res(data);
-			});
-			setTimeout(() => {
-				rej(new Error('Did not receive delete event within 10 seconds'));
-			}, 30000);
-		});
-
 		const age = Math.round(Math.random() * 100);
 		const created = await struct
 			.new({
@@ -65,11 +34,6 @@ describe('Realtime Tests', () => {
 		expect(data.data.age).toBe(age);
 		const id = data.id || '';
 
-		const createdData = await createPromise;
-		expect(createdData).toBeDefined();
-		expect(createdData.data.name).toBe('Realtime Test');
-		expect(createdData.data.age).toBe(age);
-		expect(createdData.id).toBe(id);
 
 		const updated = await data
 			.update((data) => ({
@@ -81,17 +45,9 @@ describe('Realtime Tests', () => {
 		if (updated.pending) throw new Error('Update is pending');
 		if ('error' in updated) throw new Error('Update failed: ' + updated.error.message);
 
-		const updatedData = await updatePromise;
-		expect(updatedData).toBeDefined();
-		expect(updatedData.data.name).toBe('Realtime Test');
-		expect(updatedData.data.age).toBe(age + 1);
-		expect(updatedData.id).toBe(id);
 
 		const deleted = await data.delete().await().unwrap();
 		if (deleted.pending) throw new Error('Delete is pending');
 		if ('error' in deleted) throw new Error('Delete failed: ' + deleted.error.message);
-
-		const deletedData = await deletePromise;
-		expect(deletedData).toBeDefined();
 	}, 30000);
 });
