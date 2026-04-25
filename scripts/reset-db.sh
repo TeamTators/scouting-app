f1996908bbf7374ae827e84821fb68c773c365c0
tsaxking <taylorking206@u.boisestate.edu>
2026-04-25 18:49:55 +0000

    enable rls policies

--- scripts/reset-db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-psql -U postgres -d postgres -f supabase/roles.sql
-psql -U postgres -d postgres -f supabase/extensions.sql
-psql -U postgres -d postgres -f supabase/schema.sql
--- scripts/supabase/sb-migration-push.ts
+++
@@ -1,8 +0,0 @@
-import { runTask } from '../../src/lib/server/utils/task';
-import { dbUrl } from '../../src/lib/server/services/supabase';
-
-export default async () => {
-	const res = await runTask('npx', 'supabase', 'db', 'push', '--db-url', dbUrl).unwrap();
-
-	console.log(res);
-};
