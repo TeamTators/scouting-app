888f54e598dc1ffc609198d8a7037bae4abc0b8a
tsaxking <taylorking206@u.boisestate.edu>
2026-04-26 05:03:59 +0000

    log

--- src/lib/server/services/supabase.ts
+++ src/lib/server/services/supabase.ts
@@ -3,6 +3,8 @@ import { type SchemaName } from '../../types/supabase-schema';
 import { type DB } from '../../types/supabase';
 import env from '../utils/env';
 
+console.log('Loaded environment: ', env);
+
 export default createClient<DB>(env.SB_PROJECT_URL, env.SB_SECRET_KEY, {
 	db: {
 		schema: env.SB_SCHEMA as SchemaName
