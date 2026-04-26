860f79b049c937d4e94621bd123e22217c3052e0
tsaxking <taylorking206@u.boisestate.edu>
2026-04-26 05:09:29 +0000

    rm sb -> env

--- scripts/supabase/sb-db.ts
+++ scripts/supabase/sb-db.ts
@@ -1,8 +1,8 @@
 import { runTask } from '../../src/lib/server/utils/task';
-import { sb } from '../../src/lib/server/services/supabase';
+import env from '../../src/lib/server/utils/env';
 
 export default async (...args: string[]) => {
-	const res = await runTask('npx', 'supabase', ...args, '--db-url', sb.db_url).unwrap();
+	const res = await runTask('npx', 'supabase', ...args, '--db-url', env.SB_DB_URL).unwrap();
 
 	console.log(res);
 };
