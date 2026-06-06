9cee6f153b224bd55944bf35a122bf6c83f77469
tsaxking <taylorking206@u.boisestate.edu>
2026-06-06 00:53:44 +0000

    add policy

--- scripts/supabase/sb-pull.ts
+++ scripts/supabase/sb-pull.ts
@@ -2,5 +2,18 @@ import env from '../../src/lib/server/utils/env';
 import sbDb from './sb-db';
 
 export default async (...args: string[]) => {
-	await sbDb('db', 'pull', '--schema', env.SB_SCHEMAS.join(','), ...args);
+	const schemas = Array.from(
+		new Set([
+			...env.SB_SCHEMAS,
+			'realtime',
+		])
+	);
+
+	await sbDb(
+		'db',
+		'pull',
+		'--schema',
+		schemas.join(','),
+		...args
+	);
 };
