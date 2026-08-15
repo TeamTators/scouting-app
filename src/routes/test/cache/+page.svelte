bef04d48b57beb3ca0eee277e6d64a63f16075d4
tsaxking <taylorking206@u.boisestate.edu>
2026-08-15 19:11:19 +0000

    delete cache works

---
+++ src/routes/test/cache/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Grid from '$lib/components/general/Grid.svelte';
+    const { data } = $props();
+
+    const Test = $derived(data.Test);
+    const all = $derived(data.query.reactive);
+</script>
+
+<button class="btn btn-primary" type="button" onclick={async () => {
+    Test.new({
+        name: `Test ${Math.random()}`,
+        age: Math.round(Math.random() * 100)
+    })
+}}>
+    New
+</button>
+<Grid 
+    data={all}
+    opts={{
+        columnDefs: [
+            {
+                field: 'raw.name',
+                headerName: 'Name'
+            },
+            {
+                field: 'raw.age',
+                headerName: 'Age',
+            }
+        ]
+    }}
+    height="400px"
+/>
---
+++ src/routes/test/cache/+page.ts
@@ -0,0 +1,19 @@
+import { SupaStruct } from '$lib/services/supabase/supastruct.svelte.js';
+
+export const load = async (event) => {
+    const parent = await event.parent();
+    
+    const Test = SupaStruct.get({
+        client: parent.supabase,
+        schema: 'test',
+        table: 'test',
+    });
+
+    const query = Test.all();
+    query.sync(1000 * 60 * 5); // Sync every 5 minutes
+
+    return {
+        query,
+        Test,
+    }
+};
